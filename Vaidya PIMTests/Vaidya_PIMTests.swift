dcf3fa84adf6da0ec6e25f1e11bfd6bd0d2886ca
rpvaidya <raghavendra.vaidya@gmail.com>
2016-06-23 07:59:12 +0530

    Initial Commit

---
+++ Vaidya PIMTests/Vaidya_PIMTests.swift
@@ -0,0 +1,36 @@
+//
+//  Vaidya_PIMTests.swift
+//  Vaidya PIMTests
+//
+//  Created by Vaidya, Raghavendra (GE Global Research) on 6/23/16.
+//  Copyright © 2016 Vaidya, Raghavendra (GE Global Research). All rights reserved.
+//
+
+import XCTest
+@testable import Vaidya_PIM
+
+class Vaidya_PIMTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
